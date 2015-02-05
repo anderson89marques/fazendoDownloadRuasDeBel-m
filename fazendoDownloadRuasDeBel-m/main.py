d29744e6dd7c7be0066ceaeedc9bc00c18bbf67b
Anderson Marques <andersonmarques@acceptiondev.com.br>
2015-02-05 15:55:51 -0300

    segundo commit :p

--- fazendoDownloadRuasDeBel-m/main.py
+++ fazendoDownloadRuasDeBel-m/main.py
@@ -1,17 +1,19 @@
 __author__ = 'andersonmarques'
 import urllib.request
-
+import re
 response = urllib.request.urlopen("http://kekanto.com.br/ruas/pa/belem")
 html = response.read().decode("utf8")
 
 start = 0
-while start < 0:
+while start >= 0:
     start = html.find("<a href=", start)
     start2 = html.find(">", start) + 1
     end = html.find("</a>", start2)
-
+    start = end
     nome_rua = html[start2:end]
     if nome_rua.strip() == "Adicionar um Lugar":
         break
-    start = end
-    print("Response: %s" % nome_rua)
+
+    #existem outros partes do html que batem com o padrão acima e esses casos são tratados aqui.
+    if not re.search("est", nome_rua):
+        print("Response: %s" % nome_rua)
